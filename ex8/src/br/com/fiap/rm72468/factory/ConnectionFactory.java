23cdbc88a970013ac4dd6fd89ebf5e32c6e8ad97
Gabriel Lucas de Toledo Ribeiro <gabrielltr84@gmail.com>
2016-05-03 22:44:32 -0300

    exercicio 8 corrigido

--- ex8/src/br/com/fiap/rm72468/factory/ConnectionFactory.java
+++ ex8/src/br/com/fiap/rm72468/factory/ConnectionFactory.java
@@ -3,24 +3,30 @@ package br.com.fiap.rm72468.factory;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Logger;
+
+import com.mysql.jdbc.log.Log;
 
 public class ConnectionFactory {
 
 	public static Connection getConnection() throws SQLException{
-		
+		System.out.println("Abrindo mysql.jdbc.Driver");
 		try {
 			   Class.forName("com.mysql.jdbc.Driver");
 		} 
 		catch (ClassNotFoundException ex){ }
 		
 		String server = "localhost";
-		String database = "test";
+		String database = "ex8";
 				
 		String url = "jdbc:mysql://" + server + "/" + database;
 		String user = "root";
-		String senha = "";
+		String senha = "1234";
+
+		System.out.println("Carregando conexão");
 
 		Connection conn = DriverManager.getConnection(url, user, senha);
+		System.out.println("Ok.");
 
 		return conn;
 	}
